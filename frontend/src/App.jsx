327f5e788873b490dc6f183d4309918a49c0edea
Raviraj Khopade <rkhopade@hawk.iit.edu>
2024-06-18 11:07:40 -0500

    basic page designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello!
-    </h1>
+    <>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </>
   )
 }
 
